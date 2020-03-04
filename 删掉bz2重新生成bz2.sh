f7bb56fee3732aca3d59448eebcd363dd63b20cb
zk1006 <zk1006@live.cn>
2020-03-05 03:23:29 +0800

    11

--- 删掉bz2重新生成bz2.sh
+++ 删掉bz2重新生成bz2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/zk/tools/repo/Packages.bz2
+rm -rf /Users/zk/tools/repo/Packages.bz2
 bzip2 Packages
 dpkg-scanpackages -m ./debs > Packages
